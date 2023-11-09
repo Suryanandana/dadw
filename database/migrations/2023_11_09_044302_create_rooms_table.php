fcf7b76eafd720bcb0e7a49a5626744e859d8a00
AriPandita <aripandita2@gmail.com>
2023-11-09 12:52:58 +0800

    setup users&rooms migration

--- database/migrations/2023_11_09_044302_create_rooms_table.php
+++ database/migrations/2023_11_09_044302_create_rooms_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('level', ['admin', 'staff', 'customer']);
-            $table->rememberToken();
+            $table->string('room_name', 50);
+            $table->string('category', 50);
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rooms');
     }
 };
