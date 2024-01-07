e392aefa73e955101e53a5d05ca04dc08878791f
AriPandita <aripandita2@gmail.com>
2024-01-07 16:20:21 +0800

    Change update form button text in admin account section

--- resources/views/admin/account/admin.blade.php
+++ resources/views/admin/account/admin.blade.php
@@ -464,8 +464,8 @@
                                 {{-- button --}}
                                 <div class="flex items-center space-x-4">
                                     <button type="submit"
-                                        class="text-white bg-green-400 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-400 dark:hover:bg-green-500 dark:focus:ring-green-600">Update
-                                        product</button>
+                                        class="text-white bg-green-400 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-400 dark:hover:bg-green-500 dark:focus:ring-green-600">Save
+                                    </button>
                                     <button type="button"
                                         class="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
                                         <svg class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
